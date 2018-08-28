d6ce54e8b3de59fd2da4bba567dece06daee8e2f
Thatiane Pisco <thatianepisco@gmail.com>
2018-08-28 17:21:28 +0100

    Code cleanup

--- client/src/app/core/VrScreenView.js
+++ client/src/app/core/VrScreenView.js
@@ -29,46 +29,58 @@ export class VrScreenView {
     };
 
     [addTitleContainer](){
+
         this.titleView = this[createTitleContainer]();
         this.container.appendChild(this.titleView);
+
     };
 
     [addInstructionContainer](){
+
         this.instructionView = this[createInstructionContainer]()
         this.container.appendChild(this.instructionView);
+
     };
 
     [addCloseButton](){
+
         this.closeButton = this[createCloseButton]();
         this.container.appendChild(this.closeButton);
+
     };
 
     [createCloseButton](){
+
         let view = document.createElement("button"); 
         view.className= "close-button";
         view.innerText = "OK";
         return view;
+
     };
 
     [createScreenContainer](){
+
         let view = document.createElement("div"); 
-        view.id= "vr-screen";
+        view.id = "vr-screen";
         this.container = document.createElement("div"); 
         this.container.className = "inner-container";
         return view;
+
     };
 
     [createTitleContainer](){
+
         let view = document.createElement("h1"); 
         view.className= "title";
         view.innerText = "VR CARDBOARD";
         return view;
+
     };
 
     [createInstructionContainer](){
 
         let view = document.createElement("p"); 
-        view.className= "instruction";
+        view.className = "instruction";
         view.innerText = "Scan with your smartphone to access the VR Mode.";
         return view;
 
@@ -83,29 +95,27 @@ export class VrScreenView {
     };
 
     [addQRCode](){
+
        this.qrCode = this[createQRCode]();
        this.container.appendChild(this.qrCode);
-       var qr = qrcode(4,"L");
+       var qr = qrcode(0,"L");
        qr.addData(window.location.href.split("?")[0] + "?vr=true");
        qr.make();
        this.qrCode.innerHTML = qr.createImgTag();
-       /*var qrcodeElement = new QRCode(this.qrCode, {
-        text: window.location.href.split("?")[0] + "?vr=true",
-        width: 256,
-        height: 256,
-        colorDark : "#000000",
-        colorLight : "#ffffff"
-        });*/
+
     };
 
     addActionMode(_action){
+
         this.closeButton.addEventListener("click", _action);
+        this.view.addEventListener("click", _action);
+
     };
 
     hide(){
 
         document.body.removeChild(this.view);
-        
+
     };
 
     show(){
