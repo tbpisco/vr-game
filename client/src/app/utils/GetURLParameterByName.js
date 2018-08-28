bdd809fc222b5bd9a6eb77bc5bf1bd7e803db528
Thatiane Pisco <thatianepisco@gmail.com>
2018-08-28 11:16:48 +0100

    Code cleanup

--- client/src/app/utils/GetURLParameterByName.js
+++ client/src/app/utils/GetURLParameterByName.js
@@ -1,19 +1,17 @@
 export class GetURLParameterByName {
 
 	constructor() {
-
-        //show an error
-        
+        throw new Error("GetURLParameterByName can not be instantiated.");
 	};
 
 	static get(name, url){
-
 		if (!url) url = window.location.href;
+		if (!name) return null;
 	    name = name.replace(/[\[\]]/g, "\\$&");
 	    let regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
 	        results = regex.exec(url);
 	    if (!results) return null;
-	    if (!results[2] || results[2] == "undefined" || results[2] == "null") return '';
+	    if (results[2] == "undefined" || results[2] == "null") return '';
 	    return decodeURIComponent(results[2].replace(/\+/g, " "));
 	}
 }
--- client/src/app/utils/GetURLParameterByName.spec.js
+++ client/src/app/utils/GetURLParameterByName.spec.js
@@ -8,6 +8,10 @@ var url_2 = "http://localhost:8080/index.html?vr=undefined";
 var url_3 = "http://localhost:8080/index.html?vr=null";
 var url_4 = "http://localhost:8080/index.html?vr=";
 var url_5 = "http://localhost:8080/index.html?vr=true&vr=false";
+var url_6 = "http://localhost:8080/index.html?vr=null&vr=null";
+var url_7 = "http://localhost:8080/index.html?vr=null&vr=undefined";
+var url_8 = "http://localhost:8080/index.html?vr=&vr=";
+var url_9 = "http://localhost:8080/index.html?name=Ada+Lovelace+%26+Margaret+Hamilton";
 
 describe('GetURLParameterByName', function() {
 
@@ -23,11 +27,25 @@ describe('GetURLParameterByName', function() {
   
   });
 
+  it('should return null when the arguments are invalid', function() {
+    
+    expect(GetURLParameterByName.get()).to.be.null;
+  
+  });
+
   it('should return empty string when there is no value', function() {
     
+    expect(GetURLParameterByName.get("vr", url_4)).to.equal("");
+    expect(GetURLParameterByName.get("vr", url_8)).to.equal("");
+  
+  });
+
+  it('should return empty string when the parameter is equals undefined or null', function() {
+    
+    expect(GetURLParameterByName.get("vr", url_6)).to.equal("");
+    expect(GetURLParameterByName.get("vr", url_7)).to.equal("");
     expect(GetURLParameterByName.get("vr", url_2)).to.equal("");
     expect(GetURLParameterByName.get("vr", url_3)).to.equal("");
-    expect(GetURLParameterByName.get("vr", url_4)).to.equal("");
   
   });
 
@@ -35,6 +53,19 @@ describe('GetURLParameterByName', function() {
     
     expect(GetURLParameterByName.get("vr", url_5)).to.equal("true");
     expect(GetURLParameterByName.get("vr", url_5)).to.not.equal("false");
+  
+  });
+
+  it('should return decoded values', function() {
+    
+    expect(GetURLParameterByName.get("name", url_9)).to.equal("Ada Lovelace & Margaret Hamilton");
+  
+  });
+  
+  it('should throw an Error when instantiate GetURLParameterByName class', function() {
+    
+    expect(function(){new GetURLParameterByName()}).to.throw(Error);
+    expect(function(){new GetURLParameterByName()}).to.throw("GetURLParameterByName can not be instantiated.");
   
   });
   
